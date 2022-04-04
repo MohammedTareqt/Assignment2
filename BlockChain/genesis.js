77460c861ab6934f63d58d8ce82565549657e604
MohammedTareqt <102997934+MohammedTareqt@users.noreply.github.com>
2022-04-04 23:59:43 +0300

    Add files via upload

---
+++ BlockChain/genesis.js
@@ -0,0 +1,8 @@
+const GENESIS_DATA = {
+
+    timestamp: Date.now(),
+    lastHash: 'hash code For example: 64b7edc786326651e031a4d12d9838d279571946d8c9a5d448c70db94b0e143f ',
+    hash: 'Hash Code same as above',
+    data: 'Himanshu'
+};
+module.exports = { GENESIS_DATA };
